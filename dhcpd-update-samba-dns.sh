7efd4d5566f77349bf7c78263150d04ae0d8a28a
Andrey Zvorygin <zv0r@ya.ru>
2020-07-23 14:46:07 +0300

    kea compatible

--- dhcpd-update-samba-dns.sh
+++ dhcpd-update-samba-dns.sh
@@ -1,14 +1,29 @@
+  GNU nano 4.8                                                                           /etc/kea/dhcpd-update-samba-dns.sh                                                                            Modified
 #!/bin/bash
-# Begin dhcpd-update-dns.sh
 
-. /etc/dhcpd/dhcpd-update-samba-dns.conf || exit 1
+[ "${KEA_FAKE_ALLOCATION}" = "0" ] || exit 1
+. /etc/kea/dhcpd-update-samba-dns.conf || exit 1
+. /etc/environment || exit 1
 
-ACTION=$1
-IP=$2
-HNAME=$3
+sleep 10
 
-export KRB5CC KEYTAB DOMAIN REALM PRINCIPAL NAMESERVER ZONE ACTION IP HNAME
+HNAME=`echo "$KEA_LEASE4_HOSTNAME" | sed "s/\..*$//"`
+if [ -n "$1" ] && [ -n "${HNAME}" ] && [ -n "${KEA_LEASE4_ADDRESS}" ]; then
+    case $1 in
+        lease4_select | lease4_renew)
+                ACTION=ADD
+        ;;
+        lease4_expire | lease4_release)
+                ACTION=DEL
+        ;;
+        *)
+                echo "Error: invalid action \"ACTION\"."  exit 3
+        ;;
+    esac
+    IP=$KEA_LEASE4_ADDRESS
 
-/usr/bin/samba-dnsupdate.sh -m &
+    export PATH KRB5CC KEYTAB DOMAIN REALM PRINCIPAL NAMESERVER ZONE ACTION IP HNAME
 
-# End dhcpd-update-samba-dns.sh
+    /etc/kea/samba-dnsupdate.sh -m &
+
+fi
